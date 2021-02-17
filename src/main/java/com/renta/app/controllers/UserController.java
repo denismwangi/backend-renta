19db7438d5ff196393d04152a30f9257be46c206
denismwangi <denniske992@gmail.com>
2021-02-17 16:22:58 +0300

    user operations

---
+++ src/main/java/com/renta/app/controllers/UserController.java
@@ -0,0 +1,102 @@
+package com.renta.app.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.renta.app.exception.ResourceNotFoundException;
+import com.renta.app.models.User;
+import com.renta.app.repository.UserRepository;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	//get all the users
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		
+		return userRepository.findAll();
+	}
+	
+	//create user
+	
+	//@PostMapping("/users")
+	//public User createUser(@RequestBody User user) {
+		
+	//	return userRepository.save(user);
+		
+	//}
+	
+	//get user by id
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable Long id){
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+		return ResponseEntity.ok(user);
+				
+	}
+	
+	//update user
+	
+	@PutMapping("/users/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable Long id , @RequestBody User userDetails){
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("user does ot exist with id:" + id));
+		
+		user.setFirstname(userDetails.getFirstname());
+		user.setLastname(userDetails.getLastname());
+		user.setUsername(userDetails.getUsername());
+		user.setEmail(userDetails.getEmail());
+		user.setPassword(userDetails.getPassword());
+		
+		User updateUser = userRepository.save(user);
+		return ResponseEntity.ok(updateUser);
+	}
+	
+	
+	//delete user
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<Map<String , Boolean>> deleteUser(@PathVariable Long id){
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("user does not exist with id: " +1d));
+		userRepository.delete(user);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/main/java/com/renta/app/security/WebSecurityConfig.java
+++ src/main/java/com/renta/app/security/WebSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.renta.app.security;
 
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -59,8 +63,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
+			//.antMatchers("api/v1/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring().antMatchers("/api/v1/users/**");
+	    //web.ignoring().antMatchers("/api/v1/user{id}");
+	    //web.ignoring().antMatchers("/api/v1/user");
+	}
+	
 }
